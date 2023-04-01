ebd9acb9a805e527e49ec62de18de6a5ffb906da
AnnikaGeng <annika.geng@foxmail.com>
2023-04-01 16:46:11 +0100

    logo position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,12 +11,12 @@ const Navbar = () => {
 
   return (
     <nav className={`$(styles.paddingX) w-full flex items-center py-5 absolute top-[-1] z-20 bg-primary`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto z-20'>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto z-20 '>
         <Link to='/' className='flex items-center gap-2' onClick={() => {
           setActive("");
           window.scrollTo(0, 0);
         }}>
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain ml-6'/>
           <p className='text-black text-[18px] font-bold cursor-pointer flex'>Yi &nbsp;
           <span className='sm:block hidden'>| Fullstack 
           Developer</span></p>
@@ -33,10 +33,10 @@ const Navbar = () => {
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-end items-center z-20'>
-            <img src={toggle ? close : menu} alt="menu" 
+            {/* <img src={toggle ? close : menu} alt="menu" 
             className='w-[28px] h-[28px] object-contain cursor-pointer'
-            onClick={() => setToggle(!toggle)}  />
-            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+            onClick={() => setToggle(!toggle)}  /> */}
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-20 rounded-xl`}>
               <ul className='list-none flex justify-end items-start flex-col gap-4'>
                 {navLinks.map((link) => (
                   <li key={link.id} 
